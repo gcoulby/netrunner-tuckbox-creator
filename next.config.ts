4990ccbb3bd4249a663fccf91b39d97906576b8a
Graham <gcoulby@gmail.com>
2025-02-16 22:01:49 +0000

    image fix attempt

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,8 @@ let assetPrefix = ''
 let basePath = ''
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `/netrunner-tuckbox-creator/`
+  basePath = `/netrunner-tuckbox-creator`
 }
 
 const nextConfig = {
